70b5fa55d4a17ab347083ee204a23145dc930e5a
Rahul Rudragoudar <rr83019@gmail.com>
2018-05-29 17:54:49 +0530

    Cleaned install.sh
    Replaced apt-get with apt
    Removed extra empty lines
    Clubbed and improved comments

--- install.sh
+++ install.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
+#Updating system packages
+apt --yes update
+apt --yes upgrade
 
-#Updating packages
+#Installing dnsmasq and ubuntu theme for LTSP display manager
+apt --yes --install-recommends install dnsmasq ldm-ubuntu-theme
 
-apt-get --yes update
-apt-get --yes upgrade
-
-#Installing dependencies
-
-apt-get --yes --install-recommends install dnsmasq ldm-ubuntu-theme
-apt-get --yes --install-recommends install ltsp-server
+#Installing LTSP server and client
+apt --yes --install-recommends install ltsp-server
 export DEBIAN_FRONTEND=noninteractive
-apt-get -yq --yes --install-recommends install ltsp-client
-apt-get --yes install epoptes epoptes-client
+apt -yq --yes --install-recommends install ltsp-client
 
-
-#Adding vagrant user to group epoptes
-
+#Installing epoptes and adding user 'vagrant' to the group
+apt --yes install epoptes epoptes-client
 gpasswd -a ${SUDO_USER:-$USER} epoptes
 
 #Updating kernel
-
 echo 'IPAPPEND=3' >> /etc/ltsp/update-kernels.conf
 /usr/share/ltsp/update-kernels
 
 #configure dnsmasq
-
 ltsp-config dnsmasq
 
 #Creating lts.conf
-
 ltsp-config lts.conf
 
-#Installing additional software
-
-apt-get --yes install edubuntu-desktop
-apt-get --yes install ubuntu-edu-preschool ubuntu-edu-primary ubuntu-edu-secondary ubuntu-edu-tertiary
+#Installing Edubuntu programs
+apt --yes install edubuntu-desktop
+apt --yes install ubuntu-edu-preschool ubuntu-edu-primary ubuntu-edu-secondary ubuntu-edu-tertiary
 
 #Installing ltsp-manager
-
 add-apt-repository ppa:ts.sch.gr
-apt-get --yes update
-apt-get --yes install ltsp-manager
+apt --yes update
+apt --yes install ltsp-manager
 
 #Creating client image
-
 ltsp-update-image --cleanup /
