7c51d065ca92dab13c1c013326ccf86bca48c856
Deepanshu Gajbhiye <gajbhiye.deepanshu@gmail.com>
2018-06-02 04:12:06 +0530

    Making ltsp server flexible (#31)
    * making flexible ltsp server
    
    added setting.sh It is loaded by install.sh that sets the mode of operation on ltsp server
    
    * updated Vagrantfile install.sh
    
    use of source implemented in install.sh. settings.sh file is loaded in Vagrantfile. dhcp-range takes value from settings.sh. updated README.md
    
    * updated install.sh Vagrantfile
    
    use of load implemented in Vagrantfile. Also improved readibility of the files.
    
    * automatic bride interface selection
    
    made some changes in Vagrantfile now it does not ask for which interface to bridge to. Made changes in readme.md file accordingly
    
    * updated install.sh and Vagrantfile
    
    used LAN_ip. Removed hardcoded number. merged 2 sed into one. removed elif condition
    
    * fetch interface name
    
    added a line in Vagrantfile that scrapes the name of interface connected to the internet
    
    * using ip route
    
    using ip route instead of route and grep intead of awk

--- Vagrantfile
+++ Vagrantfile
@@ -1,11 +1,13 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
+load "settings.sh"
+INTERFACE = %x(  ip route | grep default | cut -d' ' -f5 )
 
 Vagrant.configure("2") do |config|
   
   config.vm.box = "ubuntu/bionic64"
 	
-  config.vm.network "public_network", ip: "192.168.1.18", netmask: "255.255.255.0", virtualbox_intnet: "eno1"
+  config.vm.network "public_network", ip: LAN_IP, netmask: "255.255.255.0", bridge: INTERFACE.strip
   
   config.vm.provider "virtualbox" do |virtualbox|
 	  # Enable promiscuous mode
@@ -16,6 +18,7 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", path: "install.sh"
 
 end
+
 
 
 
--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 #Updating packages
-
 apt-get --yes update
 apt-get --yes upgrade
 
 #Installing dependencies
-
 apt-get --yes --install-recommends install dnsmasq ldm-ubuntu-theme
 apt-get --yes --install-recommends install ltsp-server
 export DEBIAN_FRONTEND=noninteractive
@@ -15,33 +13,49 @@ apt-get --yes install epoptes epoptes-client
 
 
 #Adding vagrant user to group epoptes
-
 gpasswd -a ${SUDO_USER:-$USER} epoptes
 
 #Updating kernel
-
 echo 'IPAPPEND=3' >> /etc/ltsp/update-kernels.conf
 /usr/share/ltsp/update-kernels
 
 #configure dnsmasq
-
 ltsp-config dnsmasq
 
 #Creating lts.conf
-
 ltsp-config lts.conf
 
 #Installing additional software
-
 apt-get --yes install edubuntu-desktop
 apt-get --yes install ubuntu-edu-preschool ubuntu-edu-primary ubuntu-edu-secondary ubuntu-edu-tertiary
 
 #Installing ltsp-manager
-
 add-apt-repository ppa:ts.sch.gr
 apt-get --yes update
 apt-get --yes install ltsp-manager
 
 #Creating client image
-
 ltsp-update-image --cleanup /
+
+#source setting.sh
+source /vagrant/settings.sh
+
+#setting dhcp-range
+sed -i /etc/dnsmasq.d/ltsp-server-dnsmasq.conf \
+-e "/^dhcp-range=.*,8h\$/ c dhcp-range=${NETWORK}.20,${NETWORK}.250,8h"
+
+#Setting mode of operation of ltsp server
+if [[ ${STANDALONE,,} == "yes" ]]; then
+	echo "LTSP server will be in standalone mode of operation"	
+	echo "LTSP server will provide DHCP services.."	
+	sed -i /etc/dnsmasq.d/ltsp-server-dnsmasq.conf -e "/192.168.1.0,proxy\$/ c \
+#dhcp-range=${NETWORK}.0,proxy" -e "/10.0.2.0,proxy\$/ c #dhcp-range=10.0.2.0,proxy"
+else
+	echo "LTSP server will be in Non-standalone mode of operation"	
+	echo "There is an existing DHCP server running"
+	echo "LTSP server won't provide DHCP services.."
+	sed -i /etc/dnsmasq.d/ltsp-server-dnsmasq.conf -e "/192.168.1.0,proxy\$/ c \
+dhcp-range=${NETWORK}.0,proxy" -e "/10.0.2.0,proxy\$/ c dhcp-range=10.0.2.0,proxy"
+fi
+
+service dnsmasq restart
