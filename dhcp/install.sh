d29bee45aae086c722b5149968cfdd88f86e4154
d78ui98 <gajbhiye.deepanshu@gmail.com>
2018-06-04 14:51:33 +0530

    fixed dhcp server
    provision script and vagrantfile now loads network address from settings.sh also Vagrantfile automatically detects network interface

--- dhcp/install.sh
+++ dhcp/install.sh
@@ -1,9 +1,13 @@
 #/bin/bash
 
+source /vagrant/settings.sh
+
 #installation
 apt --yes update
 apt --yes --install-recommends install dnsmasq
 
 #configuration
-echo "dhcp-range=tftp,192.168.1.250,192.168.1.254" >> /etc/dnsmasq.conf 
+echo "dhcp-range=tftp,${NETWORK}.250,${NETWORK}.254" >> /etc/dnsmasq.conf 
 service dnsmasq restart
+	
+
