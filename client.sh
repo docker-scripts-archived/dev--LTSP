998731fc4a54418e42be3bad879813f6a83806a0
d78ui98 <gajbhiye.deepanshu@gmail.com>
2018-06-10 02:44:25 +0530

    enhancements in client.sh
    Made client.sh less complex, readable and added usage function

--- client.sh
+++ client.sh
@@ -1,47 +1,61 @@
 #!/bin/bash
-#### This is a simple script that creates LTSP clients 
-#### These clients are diskless and only boot from network
-#### So make sure LTSP server is running before executing script
+#################################################
+#  Simple script that creates LTSP clients      #
+#  These clients can only boot from network     #
+#  LTSP server is running before running script	#
+#################################################
 
-cmd=$1
 clientname=''
+INTERFACE=$(ip route |grep default |cut -d' ' -f5)
+
+usage(){
+cat <<-END
+
+DESCRIPTION:
+
+    Script that creates LTSP client.
+
+USAGE:
+		
+    Simply run the script and enter the name 
+    of the client when prompted.
+						
+OPTIONS:
+
+    -h, --help
+    Display help
+
+END
+}
 
 create_client(){
 	
-	echo -n "Enter any name for LTSP client : "
-	read clientname
-	VBoxManage createvm --name "$clientname" --register
-	cd $HOME/VirtualBox\ VMs/$clientname
-	VBoxManage modifyvm "$clientname" --memory 1024 --acpi on --boot1 net --boot2 none --boot3 none --boot4 none --nic1 bridged --bridgeadapter1 eno1 --nicpromisc1 allow-all
-	VBoxManage createhd --filename "$clientname.vdi" --size 1
-	VBoxManage storagectl "$clientname" --name "IDE Controller" --add ide
-	VBoxManage startvm "$clientname"
+    echo -n "Enter any name for LTSP client : "
+    read clientname
+    echo "Using ${INTERFACE}.."	
+    VBoxManage createvm --name "${clientname}" --register
+    cd "$HOME/VirtualBox VMs/${clientname}"
+    VBoxManage modifyvm "${clientname}" \
+        --memory 1024 \
+        --acpi on \
+        --boot1 net \
+        --boot2 none \
+        --boot3 none \
+        --boot4 none \
+        --nic1 bridged \
+        --bridgeadapter1 $INTERFACE \
+        --nicpromisc1 allow-all 
+    VBoxManage createhd --filename \
+        "$HOME/VirtualBox VMs/${clientname}/${clientname}.vdi" --size 1
+    VBoxManage storagectl "${clientname}" --name "IDE Controller" --add ide
+    VBoxManage startvm "${clientname}"
 }
 
-control_client(){
-	
-  echo -n "choose an option(poweroff/reset/) : "
-	read cmd  
-	case $cmd in
-  	off|poweroff)
-			echo "powering off $clientname"
-			VBoxManage controlvm "$clientname" poweroff			
-			;;
-		pause)
-			echo "pausing off $clientname"
-			VBoxManage controlvm "$clientname" pause			
-			;;
-		reset)
-			echo "reseting off $clientname"
-			VBoxManage controlvm "$clientname" reset			
-			;;
-		*)
-			echo "Unknown option"
-	esac
-}
 
 if [ "$1" == "" ]; then
-  create_client
-	control_client
+    create_client
 fi
 
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    usage
+fi
