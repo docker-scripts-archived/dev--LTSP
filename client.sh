62411db3e1dd06c70d3e2d8d7bfd7f958f897704
d78ui98 <gajbhiye.deepanshu@gmail.com>
2018-06-11 21:37:08 +0530

    removed unnecessary output

--- client.sh
+++ client.sh
@@ -9,10 +9,7 @@ INTERFACE=$(ip route | grep -m 1 default | cut -d' ' -f5)
 
 vmname=${1:-ltsp-client}
 
-if [ $vmname == "ltsp-client" ]; then
-    VBoxManage unregistervm $vmname --delete
-fi
-
+VBoxManage unregistervm $vmname --delete 2>/dev/null
 VBoxManage createvm --name "${vmname}" --register
 VBoxManage modifyvm "${vmname}" \
     --memory 1024 \
