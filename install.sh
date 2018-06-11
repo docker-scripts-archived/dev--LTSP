d4031d75a34be2db7949d77dc3eb4bca6cd88b8c
d78ui98 <gajbhiye.deepanshu@gmail.com>
2018-06-11 19:47:18 +0530

    removed unnecessary lines
    removed unnecessary line on non standalone mode of operation

--- install.sh
+++ install.sh
@@ -50,9 +50,6 @@ else
     echo "LTSP server will be in Non-standalone mode of operation"	
     echo "There is an existing DHCP server running"
     echo "LTSP server won't provide DHCP services.."
-    sed -i /etc/dnsmasq.d/ltsp-server-dnsmasq.conf \
-        -e "/192.168.1.0,proxy\$/ c dhcp-range=${NETWORK}.0,proxy" \
-        -e "/10.0.2.0,proxy\$/ c dhcp-range=10.0.2.0,proxy"
 fi
 
 service dnsmasq restart
