cd27dd1dcf3daf826aa04d12139fa19e6ff8976d
d78ui98 <gajbhiye.deepanshu@gmail.com>
2018-06-15 23:05:18 +0530

    updated client.sh

--- client.sh
+++ client.sh
@@ -5,8 +5,19 @@
 #  LTSP server is running before running script	#
 #################################################
 
-INTERFACE=$(ip route | grep -m 1 default | cut -d' ' -f5)
+source settings.sh
 
+if [ $TYPE == 'bridge' ]; then    
+    INTERFACE=$(ip route | grep -m 1 default | cut -d' ' -f5)
+    nic='bridged'
+    adapter='bridgeadapter1'
+else
+    INTERFACE=$(VBoxManage list hostonlyifs | grep -B 3 $NETWORK \
+        | grep Name | cut -d':' -f2)
+    nic='hostonly'
+    adapter='hostonlyadapter1'
+fi    
+    
 vmname=${1:-ltsp-client}
 
 VBoxManage unregistervm $vmname --delete 2>/dev/null
@@ -15,7 +26,7 @@ VBoxManage modifyvm "${vmname}" \
     --memory 1024 \
     --acpi on \
     --boot1 net \
-    --nic1 bridged \
-    --bridgeadapter1 $INTERFACE \
+    --nic1 $nic \
+    --$adapter $INTERFACE \
     --nicpromisc1 allow-all 
 VBoxManage startvm "${vmname}"
