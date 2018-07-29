2992609a937a1aca46d3380b52bbf701dc9b3fba
d78ui98 <gajbhiye.deepanshu@gmail.com>
2018-07-29 14:17:45 +0530

    removed uid check, added feature to create and destroy vagrant boxes, and ltsp client

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 source settings.sh
+NETWORK="$(echo $LAN_IP | cut -d'.' -f1-3)"
 DEVICE=ltsptest01
 
 help() {
@@ -10,7 +11,6 @@ This script creates virtual network adapter which can be used for bridging
 
 Usage:
 $0 [options]
-
 Options:
     start   This option will create virtual adapter
     stop    This option will destory virtual adapter
@@ -18,29 +18,48 @@ Options:
 _EOF
 }
 
-if [[ $UID != 0 ]]; then
-	echo "error: use sudo or run script as root user"
-	exit 1
-fi
-
 case $1 in
     start )
+        
         echo "creating virtual interface.."
-        modprobe dummy
-        ip link add ${DEVICE} type dummy
-        ip link set ${DEVICE} up
-        ip addr add ${NETWORK}.100/24 brd + dev ${DEVICE}
-        sed -i settings.sh -e "/^INTERFACE/ c INTERFACE=\"${DEVICE}\"" 
+        sudo modprobe dummy
+        sudo ip link add ${DEVICE} type dummy
+        sudo ip link set ${DEVICE} up
+        sudo ip addr add ${NETWORK}.100/24 brd + dev ${DEVICE}
+        sed -i settings.sh -e "/^LAN_IF/ c LAN_IF=\"${DEVICE}\"" 
+        
+        vagrant destroy -f
+        cd dhcp
+        vagrant destroy -f
+        cd -
+       
+        if [ ${STANDALONE,,} != "yes" ] ; then
+        	cd dhcp
+        	vagrant up
+        	cd -
+        fi
+        vagrant up && ./client.sh
         ;;	
     stop )
+        
         echo "destroying virtual interface.."
-        ip addr del ${NETWORK}.100/24 brd + dev ${DEVICE}
-        ip link delete ${DEVICE} type dummy
-        sed -i settings.sh -e '/^INTERFACE/ c INTERFACE="" '  
-        rmmod dummy
+        sudo ip addr del ${NETWORK}.100/24 brd + dev ${DEVICE}
+        sudo ip link delete ${DEVICE} type dummy
+        sed -i settings.sh -e '/^LAN_IF/ c LAN_IF="" '  
+        sudo rmmod dummy
+        
+        VBoxManage controlvm ltsp-client poweroff 2>/dev/null
+        sleep 0.5
+        VBoxManage unregistervm ltsp-client --delete 2>/dev/null 
+        
+        vagrant halt
+        vagrant destroy -f
+        cd dhcp/
+        vagrant destroy -f
+        cd -
         ;;
     * )
         echo "error: invalid arguments provided"
         help
         ;;
-esac    
+esac 
